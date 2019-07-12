193eb835ed815583139477b8f6050a120a8a2073
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-12 00:22:06 -0300

    arrumando a listagem de produtos, buscando na api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,86 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/26/D12-3139-026/D12-3139-026_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/26/D12-3139-026/D12-3139-026_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/26/D12-3139-026/D12-3139-026_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/26/D12-3139-026/D12-3139-026_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+  render() {
+    const { products } = this.state;
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/26/D12-3139-026/D12-3139-026_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
