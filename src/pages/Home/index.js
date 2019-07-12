32e09c63e56fbd6a83adb387b9cf424a337ba6d1
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-12 11:47:20 -0300

    melhorando o codigo referente as actions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import api from '../../services/api';
+
+import * as CartActions from '../../store/modules/cart/actions';
 import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
@@ -22,12 +25,9 @@ class Home extends Component {
   }
 
   handleAddProduct = product => {
-    const { dispatch } = this.props;
+    const { addToCart } = this.props;
 
-    dispatch({
-      type: 'ADD_TO_CART',
-      product,
-    });
+    addToCart(product);
   };
 
   render() {
@@ -57,4 +57,10 @@ class Home extends Component {
   }
 }
 
-export default connect()(Home);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Home);
