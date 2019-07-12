dbc656c1ec0da64bed93bdf184a4e574ba1f05d9
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-12 11:10:33 -0300

    listando o carrinho

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,6 @@ module.exports = {
       { extensions: ['.jsx', '.js'] }
     ],
     'import/prefer-default-export': 'off',
-    'no-console': ['error' , {allor: ['tron']}]
+    //'no-console': ['error' , {allor: ['tron']}]
   },
 };
