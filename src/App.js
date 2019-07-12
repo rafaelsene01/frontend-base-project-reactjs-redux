d893b20d7748623db38cde4af841c7b0028cb5e7
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-12 16:23:29 -0300

    implementando toast

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
-import './config/ReactotronConfig'
+import './config/ReactotronConfig';
 
 import Routes from './routes';
 import GlobalStyles from './styles/global';
@@ -17,6 +18,7 @@ function App() {
         <Header />
         <Routes />
         <GlobalStyles />
+        <ToastContainer autoClose={3000} />
       </BrowserRouter>
     </Provider>
   );
